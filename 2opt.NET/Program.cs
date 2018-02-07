57884a050551e97ac401df2efb952bb6ae566365
JP Dillingham <jpdillingham@gmail.com>
2018-02-06 18:35:44 -0600

    Stubbed classes

--- 2opt.NET/Program.cs
+++ 2opt.NET/Program.cs
@@ -4,9 +4,35 @@ namespace _2opt.NET
 {
     class Program
     {
+        Random rng = new Random();
+
+        private static int PointCount { get; set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(PointCount);
+        }
+    }
+
+    class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point()
+        {
+            X = 0.0;
+            Y = 0.0;
+        }
+    }
+
+    class Line
+    {
+        public Point[] Points { get; set; }
+
+        public Line()
+        {
+            Points = new Point[2];
         }
     }
 }
