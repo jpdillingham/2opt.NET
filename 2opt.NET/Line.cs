abd8320193bc3c9f4a114f35fb985c2f0fe71b14
JP Dillingham <jpdillingham@gmail.com>
2018-02-06 21:19:56 -0600

    Moved Line and Point classes to their own files

---
+++ 2opt.NET/Line.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2opt.NET
+{
+    public class Line
+    {
+        public Point[] Points { get; set; }
+
+        public Line()
+        {
+            Points = new Point[2];
+        }
+
+        public override string ToString()
+        {
+            return $"{Points[0]} {Points[1]}";
+        }
+    }
+}
---
+++ 2opt.NET/Point.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2opt.NET
+{
+    public class Point
+{
+    public double X;
+    public double Y;
+
+    public Point(double x, double y) { X = x; Y = y; }
+    public Point() : this(double.NaN, double.NaN) { }
+
+    public static Point operator -(Point v, Point w)
+    {
+        return new Point(v.X - w.X, v.Y - w.Y);
+    }
+
+    public static Point operator +(Point v, Point w)
+    {
+        return new Point(v.X + w.X, v.Y + w.Y);
+    }
+
+    public static double operator *(Point v, Point w)
+    {
+        return v.X * w.X + v.Y * w.Y;
+    }
+
+    public static Point operator *(Point v, double mult)
+    {
+        return new Point(v.X * mult, v.Y * mult);
+    }
+
+    public static Point operator *(double mult, Point v)
+    {
+        return new Point(v.X * mult, v.Y * mult);
+    }
+
+    public double Cross(Point v)
+    {
+        return X * v.Y - Y * v.X;
+    }
+
+    public override bool Equals(object obj)
+    {
+        var v = (Point)obj;
+        return (X - v.X).IsZero() && (Y - v.Y).IsZero();
+    }
+
+    public override string ToString()
+    {
+        return $"({X}, {Y})";
+    }
+}
+}
